78373eb5fcf27a28b75e9743524710bc0c4f1140
Stonkie <stonkie@gmail.com>
2017-06-25 17:11:42 -0400

    ADD - Added example for pulse/wait.

---
+++ LOG645_Cours7_PulseWait/LOG645_Cours7_PulseWait/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace LOG645_Cours7_PulseWait
+{
+    internal class Program
+    {
+        private class SomeJob
+        {
+            public int Id { get; set; }
+        }
+
+        private static readonly Queue<SomeJob> Jobs = new Queue<SomeJob>();
+        private static readonly Object JobsLock = new Object();
+
+        private static void Main(string[] args)
+        {
+            
+            StartConsumer();
+            StartConsumer();
+            StartConsumer();
+            StartConsumer();
+            StartConsumer();
+            StartConsumer();
+            StartConsumer();
+            StartConsumer();
+            StartConsumer();
+            StartConsumer();
+            StartConsumer();
+            StartConsumer();
+            StartConsumer();
+            StartConsumer();
+            StartConsumer();
+            StartConsumer();
+
+            Thread.Sleep(100);
+
+            StartProducer();
+        }
+
+        private static void StartProducer()
+        {
+            var producerThread = new Thread(() =>
+            {
+                for (int jobId = 0; jobId < 1000; jobId++)
+                {
+                    lock (JobsLock)
+                    {
+                        Console.WriteLine($"     Producing job {jobId}");
+                        Jobs.Enqueue(new SomeJob() {Id = jobId});
+                        Monitor.PulseAll(JobsLock);
+                    }
+                    Thread.Sleep(10);
+                }
+            });
+
+            producerThread.Start();
+        }
+
+        private static void StartConsumer()
+        {
+            var consumerThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    SomeJob job;
+
+                    lock (JobsLock)
+                    {
+                        while (!Jobs.Any())
+                        {
+                            Monitor.Wait(JobsLock);
+                        }
+
+                        job = Jobs.Dequeue();
+                    }
+
+                    Console.WriteLine($"Consuming job {job.Id}");
+                    Thread.Sleep(100);
+                }
+            });
+
+            consumerThread.Start();
+        }
+    }
+}
