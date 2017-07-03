e6442a2e858a5c19eb09bfd0b10980df3c43e9cf
Stonkie <stonkie@gmail.com>
2017-07-03 09:09:41 -0400

    ADD - Added example for counting semaphore to limited CPU cores ressource.

---
+++ LOG645_Cours8_LimiterTroisCPU/LOG645_Cours8_LimiterTroisCPU/LimitedThreadPool.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LOG645_Cours8_LimiterTroisCPU
+{
+    public class LimitedThreadPool
+    {
+        private readonly SemaphoreSlim _semaphore;
+        
+        public LimitedThreadPool(int maxConcurrency)
+        {
+            _semaphore = new SemaphoreSlim(maxConcurrency);
+        }
+
+        public Thread Run(Action action)
+        {
+            Thread thread = new Thread(() =>
+            {
+                _semaphore.Wait();
+
+                try
+                {
+                    action();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Boom!");    
+                }
+                finally
+                {
+                    _semaphore.Release();
+                }
+            });
+
+            thread.Start();
+
+            return thread;
+        }
+    }
+}
---
+++ LOG645_Cours8_LimiterTroisCPU/LOG645_Cours8_LimiterTroisCPU/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LOG645_Cours8_LimiterTroisCPU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Thread> threads = new List<Thread>();
+
+            LimitedThreadPool pool = new LimitedThreadPool(3);
+            
+            for (int actionIndex = 0; actionIndex < 7; actionIndex++)
+            {
+                int index = actionIndex;
+                threads.Add(pool.Run(() =>
+                {
+                    Thread.Sleep(100);
+                    Console.WriteLine($"Thread {index} finished at {DateTime.Now.ToString("o")} .");
+                }));
+            }
+
+            threads.ForEach(thread => thread.Join());
+
+            Console.WriteLine("Finished all!");
+            Console.ReadKey();
+        }
+    }
+}
